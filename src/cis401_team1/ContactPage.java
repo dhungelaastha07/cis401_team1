95de31a384f8d3dc19c6051f4df35d00a29e22e7
dhungelaastha07 <adhungel2@horizon.csueastbay.edu>
2020-07-20 19:26:26 -0700

    Demo ready

--- src/cis401_team1/ContactPage.java
+++ src/cis401_team1/ContactPage.java
@@ -1,4 +1,5 @@
 package cis401_team1;
+
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -12,21 +13,21 @@ import javax.swing.JSeparator;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
 
-public class ContactPage implements ActionListener  {
-	
+public class ContactPage implements ActionListener {
+	Client activeClient;
 	JPanel contactPanel;
 	JLabel titleLabel;
-	JButton contactName1,contactName2,contactName3,contactName4,addNewContact;
-	
-	//title 
-	ContactPage(){
+	JButton contactName1, contactName2, contactName3, contactName4, addNewContact;
+
+	// title
+	ContactPage(Client _activeClient) {
+		this.activeClient = _activeClient;
 		contactPanel = new JPanel(new GridLayout(10, 0));
 		titleLabel = new JLabel();
 		titleLabel.setText("Your Contacts");
 		titleLabel.setFont(new Font("Verdana", Font.BOLD, 18));
 		contactPanel.add(titleLabel);
-		
-		
+
 		// contactname1
 		contactName1 = new JButton("Aastha");
 		contactName1.setBackground(new Color(99, 173, 242));
@@ -35,7 +36,6 @@ public class ContactPage implements ActionListener  {
 		contactName1.addActionListener(this);
 		contactPanel.add(contactName1);
 		contactPanel.add(new JSeparator());
-		
 
 		// contactname2
 		contactName2 = new JButton("Brandy");
@@ -45,7 +45,6 @@ public class ContactPage implements ActionListener  {
 		contactName2.addActionListener(this);
 		contactPanel.add(contactName2);
 		contactPanel.add(new JSeparator());
-		
 
 		// contactname3
 		contactName3 = new JButton("Tim");
@@ -55,7 +54,6 @@ public class ContactPage implements ActionListener  {
 		contactName3.addActionListener(this);
 		contactPanel.add(contactName3);
 		contactPanel.add(new JSeparator());
-		
 
 		// contactname4
 		contactName4 = new JButton("Nicolai");
@@ -65,45 +63,42 @@ public class ContactPage implements ActionListener  {
 		contactName4.addActionListener(this);
 		contactPanel.add(contactName4);
 		contactPanel.add(new JSeparator());
-		
-		
-		//add button
+
+		// add button
 		addNewContact = new JButton("Add New Contacts");
 		addNewContact.setBackground(new Color(167, 204, 237));
 		addNewContact.setOpaque(true);
 		addNewContact.setBorderPainted(false);
 		addNewContact.addActionListener(this);
 		contactPanel.add(addNewContact);
-		
+
 		// adding padding
 		Border padding = BorderFactory.createEmptyBorder(10, 10, 10, 10);
 		contactPanel.setBorder(padding);
 	}
-	
+
 	JPanel getPanel() {
-		
+
 		return this.contactPanel;
 	}
-		
-		public void actionPerformed(ActionEvent e) {
-			String command = e.getActionCommand();
 
-			if (command.equals("Aastha")) {
-				//take user to chat with Aastha
-				System.out.print("Astha");
-			} else if (command.equals("Brandy")) {
-				//take user to chat with Brandy
-			} else if (command.equals("Tim")) {
-				//take user to chat with Tim
-			} else if (command.equals("Nicolai")) {
-				//take user to chat with Nicolai
-			} else if (command.equals("Add New Contacts")) {
-				//add new contacts page
-			} else {
-				System.out.print("Invalid");
-			}
+	public void actionPerformed(ActionEvent e) {
+		String command = e.getActionCommand();
+
+		if (command.equals("Aastha")) {
+			// take user to chat with Aastha
+			this.activeClient.setChatScreen(command);
+		} else if (command.equals("Brandy")) {
+			// take user to chat with Brandy
+		} else if (command.equals("Tim")) {
+			// take user to chat with Tim
+		} else if (command.equals("Nicolai")) {
+			// take user to chat with Nicolai
+		} else if (command.equals("Add New Contacts")) {
+			// add new contacts page
+		} else {
+			System.out.print("Invalid");
 		}
-
-		
-		
 	}
+
+}
